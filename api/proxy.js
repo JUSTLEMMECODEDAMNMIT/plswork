fae9f5d5659df660e2cd84bef95903c5a07d8d3e
JUSTLEMMECODEDAMNMIT <158492462+JUSTLEMMECODEDAMNMIT@users.noreply.github.com>
2024-08-03 15:59:28 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,10 @@ const server = createServer((req, res) => {
     const targetUrl = parsedUrl.query.target;
 
     if (targetUrl) {
-        proxy.web(req, res, { target: targetUrl, changeOrigin: true });
+        proxy.web(req, res, { target: targetUrl, changeOrigin: true }, (e) => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Something went wrong. And we are reporting a custom error message.');
+        });
     } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end('<html><body><h1>Proxy</h1><p>Use <code>?target=URL</code> to proxy a request.</p></body></html>');
