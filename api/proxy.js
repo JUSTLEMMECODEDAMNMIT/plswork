503a8ef066090a3170bb2d9bc19b5c268d67d1a6
JUSTLEMMECODEDAMNMIT <158492462+JUSTLEMMECODEDAMNMIT@users.noreply.github.com>
2024-08-03 16:19:53 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,14 +9,15 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, '../static')));
 
 // Proxy endpoint
 app.get('/proxy', (req, res) => {
     const targetUrl = req.query.target;
 
+    console.log('Received proxy request for:', targetUrl);
+
     if (targetUrl) {
-        console.log('Proxying to:', targetUrl);
         proxy.web(req, res, { target: targetUrl }, (err) => {
             if (err) {
                 console.error('Proxy error:', err);
@@ -32,12 +33,12 @@ app.get('/proxy', (req, res) => {
 // Serve static files and handle errors
 app.use((req, res, next) => {
     console.log('404 Not Found - Serving 404.html');
-    res.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, '../static', '404.html'));
 });
 
 app.use((err, req, res, next) => {
     console.error('Server error:', err);
-    res.status(500).sendFile(path.join(__dirname, 'static', '500.html'));
+    res.status(500).sendFile(path.join(__dirname, '../static', '500.html'));
 });
 
 const PORT = process.env.PORT || 8080;
