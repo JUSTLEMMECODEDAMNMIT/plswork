f24a64730797b3a487ffa59a515cf8b9ac602e74
JUSTLEMMECODEDAMNMIT <158492462+JUSTLEMMECODEDAMNMIT@users.noreply.github.com>
2024-08-03 16:13:01 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -11,23 +11,31 @@ const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Proxy endpoint
 app.get('/proxy', (req, res) => {
     const targetUrl = req.query.target;
 
     if (targetUrl) {
+        console.log('Proxying to:', targetUrl);
         proxy.web(req, res, { target: targetUrl }, (err) => {
-            console.error('Proxy error:', err);
-            res.status(500).send('Proxy error');
+            if (err) {
+                console.error('Proxy error:', err);
+                res.status(500).send('Proxy error');
+            }
         });
     } else {
+        console.log('404 Not Found - No target URL provided');
         res.status(404).send('Not Found');
     }
 });
 
+// Handle 404 for other routes
 app.use((req, res, next) => {
+    console.log('404 Not Found - Serving 404.html');
     res.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
 });
 
+// Handle errors
 app.use((err, req, res, next) => {
     console.error('Server error:', err);
     res.status(500).sendFile(path.join(__dirname, 'static', '500.html'));
