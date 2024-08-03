3493a5fd8da42239c8a5866400c255c81c00f66c
JUSTLEMMECODEDAMNMIT <158492462+JUSTLEMMECODEDAMNMIT@users.noreply.github.com>
2024-08-03 16:10:39 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,28 +1,39 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-const url = require('url');
-const proxy = httpProxy.createProxyServer({});
+import express from 'express';
+import httpProxy from 'http-proxy';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const targetUrl = parsedUrl.query.target;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const proxy = httpProxy.createProxyServer({ changeOrigin: true });
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/proxy', (req, res) => {
+    const targetUrl = req.query.target;
 
     if (targetUrl) {
-        proxy.web(req, res, { target: targetUrl, changeOrigin: true }, (e) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Something went wrong. And we are reporting a custom error message.');
+        proxy.web(req, res, { target: targetUrl }, (err) => {
+            console.error('Proxy error:', err);
+            res.status(500).send('Proxy error');
         });
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+        res.status(404).send('Not Found');
     }
 });
 
-server.on('error', (err, req, res) => {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Something went wrong. And we are reporting a custom error message.');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
 });
 
-server.listen(3000, () => {
-    console.log('Proxy server is listening on port 3000');
+app.use((err, req, res, next) => {
+    console.error('Server error:', err);
+    res.status(500).sendFile(path.join(__dirname, 'static', '500.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
