badd7d27f1aefc0ccf2aeacc614edfb2603a4151
JUSTLEMMECODEDAMNMIT <158492462+JUSTLEMMECODEDAMNMIT@users.noreply.github.com>
2024-08-03 16:35:21 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,47 +1,29 @@
-import express from 'express';
-import httpProxy from 'http-proxy';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const http = require('http');
+const httpProxy = require('http-proxy');
+const url = require('url');
+const proxy = httpProxy.createProxyServer({});
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const targetUrl = parsedUrl.query.target;
 
-const app = express();
-const proxy = httpProxy.createProxyServer({ changeOrigin: true });
-
-// Serve static files
-app.use(express.static(path.join(__dirname, '../static')));
-
-// Proxy endpoint
-app.get('/proxy', (req, res) => {
-    const targetUrl = req.query.target;
-
-    if (!targetUrl) {
-        console.error('Target URL is missing');
-        return res.status(400).send('Target URL is required');
+    if (targetUrl) {
+        proxy.web(req, res, { target: targetUrl, changeOrigin: true }, (e) => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Something went wrong. And we are reporting a custom error message.');
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
     }
-
-    console.log('Proxying request to:', targetUrl);
-
-    proxy.web(req, res, { target: targetUrl, changeOrigin: true }, (err) => {
-        if (err) {
-            console.error('Proxy error:', err);
-            res.status(500).send('Proxy error');
-        }
-    });
 });
 
-// Serve static 404 and 500 pages
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '../static', '404.html'));
+server.on('error', (err) => {
+    console.error('Error:', err);
+    // Optionally: res.writeHead(500, { 'Content-Type': 'text/plain' });
+    // res.end('Something went wrong. And we are reporting a custom error message.');
 });
 
-app.use((err, req, res, next) => {
-    console.error('Server error:', err);
-    res.status(500).sendFile(path.join(__dirname, '../static', '500.html'));
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+    console.log('Proxy server is listening on port 3000');
 });
